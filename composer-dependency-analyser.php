207b99de359b3b1df0ad131910cb2d2648ec050d
Jeroen de Graaf <gember@jero.work>
2024-10-11 17:37:12 +0200

    Ignore incorrect erros of prod dependencies only used in dev

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -11,6 +11,10 @@ $config->addPathToScan(__DIR__ . '/bin', isDev: false);
 $config->addPathToScan(__DIR__ . '/config', isDev: true);
 $config->addPathToScan(__DIR__ . '/public', isDev: false);
 
+$config->ignoreErrorsOnPackage('doctrine/doctrine-bundle', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
+$config->ignoreErrorsOnPackage('doctrine/doctrine-migrations-bundle', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
+$config->ignoreErrorsOnPackage('gember/event-sourcing-symfony-bundle', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
+
 $config->ignoreErrorsOnPackage('doctrine/dbal', [ErrorType::UNUSED_DEPENDENCY]);
 $config->ignoreErrorsOnPackage('doctrine/orm', [ErrorType::UNUSED_DEPENDENCY]);
 $config->ignoreErrorsOnPackage('symfony/dotenv', [ErrorType::UNUSED_DEPENDENCY]);
