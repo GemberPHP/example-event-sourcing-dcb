554d109577ad2ce27590557677e0f6931ac3ad05
Jeroen de Graaf <gember@jero.work>
2024-10-11 16:04:27 +0200

    Configure dependency analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+$config->addPathToScan(__DIR__ . '/bin', isDev: false);
+$config->addPathToScan(__DIR__ . '/config', isDev: true);
+$config->addPathToScan(__DIR__ . '/public', isDev: false);
+
+$config->ignoreErrorsOnPackage('doctrine/dbal', [ErrorType::UNUSED_DEPENDENCY]);
+$config->ignoreErrorsOnPackage('doctrine/orm', [ErrorType::UNUSED_DEPENDENCY]);
+$config->ignoreErrorsOnPackage('symfony/dotenv', [ErrorType::UNUSED_DEPENDENCY]);
+$config->ignoreErrorsOnPackage('symfony/flex', [ErrorType::UNUSED_DEPENDENCY]);
+$config->ignoreErrorsOnPackage('symfony/runtime', [ErrorType::UNUSED_DEPENDENCY]);
+$config->ignoreErrorsOnPackage('symfony/yaml', [ErrorType::UNUSED_DEPENDENCY]);
+
+return $config;
