d062768dceb54d28b6d7f0d6057833a42396136f
Jeroen de Graaf <gember@jero.work>
2026-08-21 16:00:38 +0200

    Bump gember packages for outbox support
    Updates dependency-contracts to ^0.5, event-sourcing to ^0.17, and
    event-sourcing-symfony-bundle to ^0.17 for the new transactional
    outbox pattern.

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -16,7 +16,6 @@ $config->ignoreErrorsOnPackage('doctrine/doctrine-migrations-bundle', [ErrorType
 $config->ignoreErrorsOnPackage('gember/event-sourcing-symfony-bundle', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
 $config->ignoreErrorsOnPackage('symfony/monolog-bundle', [ErrorType::PROD_DEPENDENCY_ONLY_IN_DEV]);
 
-$config->ignoreErrorsOnPackage('doctrine/dbal', [ErrorType::UNUSED_DEPENDENCY]);
 $config->ignoreErrorsOnPackage('doctrine/orm', [ErrorType::UNUSED_DEPENDENCY]);
 $config->ignoreErrorsOnPackage('symfony/dotenv', [ErrorType::UNUSED_DEPENDENCY]);
 $config->ignoreErrorsOnPackage('symfony/flex', [ErrorType::UNUSED_DEPENDENCY]);
